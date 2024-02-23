bb57c23ef94f222ff5c62a652abcb03951c1ab8c
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-23 10:28:08 +0100

    getting jobs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const url = "https://edwardtanguay.vercel.app/share/jobs.json";
+
 function App() {
+	const [jobs, setJobs] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(url);
+			const _jobs = response.data;
+			setJobs(_jobs);
+		})();
+	}, []);
+
 	return (
 		<>
 			<h1 className="text-3xl mb-4 text-yellow-300">Job Site</h1>
-			<p>Welcome to this site.</p>
+			<p>There are {jobs.length} jobs.</p>
+			{}
 		</>
 	);
 }
