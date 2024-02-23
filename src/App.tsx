38d60f31ce74ca2b535b4004aa4c4e41edd58327
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-23 12:08:34 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { IJob } from "./interfaces";
 import { Job } from "./components/Job";
 
 const url = "https://edwardtanguay.vercel.app/share/jobs.json";
+const jobOfTheDayId = 175;
 
 function App() {
 	const [jobs, setJobs] = useState<IJob[]>([]);
@@ -25,9 +26,21 @@ function App() {
 	return (
 		<>
 			<h1 className="text-3xl mb-4 text-yellow-300">Job Site</h1>
+
+			<h2 className="text-2xl">Job of the Day</h2>
+			<div className="mb-4">
+				<Job
+					job={jobs.find((m) => m.id === jobOfTheDayId)}
+					jobs={jobs}
+					setJobs={setJobs}
+				/>
+			</div>
+
 			<p className="mb-3">There are {jobs.length} jobs.</p>
 			{jobs.map((job) => {
-				return <Job key={job.id} job={job} jobs={jobs} setJobs={setJobs} />;
+				return (
+					<Job key={job.id} job={job} jobs={jobs} setJobs={setJobs} />
+				);
 			})}
 		</>
 	);
--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -2,34 +2,43 @@ import { IJob } from "../interfaces";
 import { FaArrowAltCircleDown } from "react-icons/fa";
 
 interface IProps {
-	job: IJob;
-	jobs: IJob[];
-	setJobs: (jobs: IJob[]) => void;
+	job: IJob | undefined;
+	jobs?: IJob[];
+	setJobs?: (jobs: IJob[]) => void;
 }
 
 export const Job = ({ job, jobs, setJobs }: IProps) => {
 	const handleToggleOpenJob = () => {
+		if (job && jobs && setJobs) {
 		job.isOpen = !job.isOpen;
-		setJobs(structuredClone(jobs));
-		// setJobs([...jobs]);
-	}
+			setJobs(structuredClone(jobs));
+		}
+		// setJobs([...jobs]); //doesn't always make a full copy, see: https://tanguay-eu.vercel.app/howtos/758
+	};
 
 	return (
 		<div className="bg-gray-900 p-3 mb-3">
-			<p className="flex gap-2 items-center">
-				<FaArrowAltCircleDown onClick={handleToggleOpenJob} className="text-xl cursor-pointer" />
-				<a
-					href={job.url}
-					className="text-xl underline text-yellow-200"
-					target="_blank"
-				>
-					{job.title}
-				</a>
-			</p>
-			{job.isOpen && (
+			{job && (
 				<>
-					<p className="text-orange-400">{job.company}</p>
-					<p>{job.title}</p>
+					<p className="flex gap-2 items-center">
+						<FaArrowAltCircleDown
+							onClick={handleToggleOpenJob}
+							className="text-xl cursor-pointer"
+						/>
+						<a
+							href={job.url}
+							className="text-xl underline text-yellow-200"
+							target="_blank"
+						>
+							{job.title} ({job.id})
+						</a>
+					</p>
+					{job.isOpen && (
+						<>
+							<p className="text-orange-400">{job.company}</p>
+							<p>{job.publicationDate}</p>
+						</>
+					)}
 				</>
 			)}
 		</div>
